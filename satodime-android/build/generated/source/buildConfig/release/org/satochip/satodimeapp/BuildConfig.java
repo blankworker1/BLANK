c2862bc704998b63c7ed5cb7a5e2d1e316098ed6
Mohit Hasan <mohitmd09@gmail.com>
2023-09-20 20:59:13 +0600

    init

---
+++ satodime-android/build/generated/source/buildConfig/release/org/satochip/satodimeapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.satochip.satodimeapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "org.satochip.blank";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 102;
+  public static final String VERSION_NAME = "0.1.2";
+}
